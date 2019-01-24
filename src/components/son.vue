ad4b5b450b6263b4999d16e7baafb14d8765ccd3
xuyanhui521 <13752228451@.163.com>
2019-01-24 18:43:00 +0800

    this. 获取元素和组件

---
+++ src/components/son.vue
@@ -0,0 +1,24 @@
+<template>
+     <div id="son">
+          <h3>这是子组件</h3>
+     </div>
+</template>
+<script>
+export default {
+     data(){
+         return {
+             week:"Friday"
+         }
+     },
+     methods:{
+         sonShow(){
+             console.log('今天周末了')
+         }
+     }
+}
+</script>
+<style>
+
+</style>
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
- 
+
+import Vue from "vue/dist/vue.js"
+import MyRef from "./components/01-ref.vue"
+Vue.component("my-ref",MyRef)
+ new Vue({
+     el:"#app",
+     data:{
+
+     },
+     components:{
+        
+     }
+ })
