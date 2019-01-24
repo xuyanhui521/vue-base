71e1e76b3fd4f8a4b93099cf559666d7722b231d
xuyanhui521 <13752228451@.163.com>
2019-01-24 11:10:27 +0800

    父组件向子组建进行简单的传传值

--- src/components/022.vue
+++ src/components/022.vue
@@ -2,7 +2,7 @@
    <div>
        <div class="e">{{msg}}</div>
       
-       <h2>{{msg1}}</h2>
+       <h2>{{flag? msg1 :msg11}}</h2>
        <h2>{{msg2}}</h2>
        <h2>{{msg3}}</h2>
        <p><button @click="change">改变父级元素传递的参数</button></p>
@@ -17,12 +17,15 @@
         props:['msg1','msg2','msg3'],
         data(){
             return {
-                msg:"022vue!"
+                flag:true,
+                msg:"022vue!",
+                msg11:this.msg1
             }
         },
         methods:{
             change(){
-                console.log(this.msg3)
+                this.flag=!this.flag
+               this.msg11="改变了"
                
             }
         },
